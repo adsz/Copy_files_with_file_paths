841d592c03a8733b3a38429cae7d8e1138233733
Adam Szydlo <adamszydlo@outlook.com>
2017-07-14 04:04:38 +0100

    Recursive list of files

--- Copy_files_with_file_paths.py
+++ Copy_files_with_file_paths.py
@@ -1,1 +1,21 @@
+source_path = "D:\Hitachi Kursy i ebooki_test"
+destination_path = "D:\Hitachi Kursy i ebooki - Rafal"
 
+import os
+file_names = []
+
+for root, dirs, files in os.walk(source_path):
+    for file in files:
+        if file.endswith(".txt"):
+             file_names.append(os.path.join(root, file))
+
+''' #ALTERNATIVE WAY 
+for dirname, dirnames, filenames in os.walk(source_path):
+    # print path to all subdirectories first.
+    for subdirname in dirnames:
+        for filename in filenames:
+            file_names.append(os.path.join(dirname, filename))
+
+'''
+
+print(file_names)
