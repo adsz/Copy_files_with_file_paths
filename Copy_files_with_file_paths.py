37992632bb0ef0b4cb8631304cff68a900bb2843
Adam Szydlo <adamszydlo@outlook.com>
2017-07-14 04:53:15 +0100

    Copy files from folder A to folder B - create subfolders - copy files

--- Copy_files_with_file_paths.py
+++ Copy_files_with_file_paths.py
@@ -1,13 +1,26 @@
 source_path = "D:\Hitachi Kursy i ebooki_test"
 destination_path = "D:\Hitachi Kursy i ebooki - Rafal"
+extension = ".txt"
 
 import os
+from shutil import copyfile
 file_names = []
 
 for root, dirs, files in os.walk(source_path):
     for file in files:
-        if file.endswith(".txt"):
-             file_names.append(os.path.join(root, file))
+        if file.endswith(extension):
+            file_old_path = os.path.join(root, file)
+            file_new_path = file_old_path.replace(source_path,destination_path)
+            new_folder = os.path.dirname(file_new_path)
+            print(new_folder)
+    
+            try:
+                os.stat(new_folder)
+            except:
+                os.makedirs(new_folder)   
+            copyfile(file_old_path,file_new_path)
+ #file_names.append(file_new_path)
+ 
 
 ''' #ALTERNATIVE WAY 
 for dirname, dirnames, filenames in os.walk(source_path):
@@ -19,3 +32,4 @@ for dirname, dirnames, filenames in os.walk(source_path):
 '''
 
 print(file_names)
+print (str(len(file_names)))
