9f1af400a4ccf897cdbd993d0b78ef6a9206a155
Adam Szydlo <adamszydlo@outlook.com>
2017-07-16 02:21:32 +0100

    Modified according to PyLint

--- Copy_files_with_file_paths.py
+++ Copy_files_with_file_paths.py
@@ -1,35 +1,40 @@
-source_path = "D:\Hitachi Kursy i ebooki_test"
-destination_path = "D:\Hitachi Kursy i ebooki - Rafal"
-extension = ".txt"
-
+'''
+Adding os module
+'''
 import os
 from shutil import copyfile
-file_names = []
 
-for root, dirs, files in os.walk(source_path):
+SOURCE_PATH = "D:\\Hitachi Kursy i ebooki"
+DESTINATION_PATH = "D:\\Hitachi Kursy i ebooki - Rafal"
+EXTENSION = ".pdf"
+
+
+FILE_NAMES = []
+
+for root, dirs, files in os.walk(SOURCE_PATH):
     for file in files:
-        if file.endswith(extension):
-            file_old_path = os.path.join(root, file)
-            file_new_path = file_old_path.replace(source_path,destination_path)
-            new_folder = os.path.dirname(file_new_path)
-            print(new_folder)
-    
-            try:
-                os.stat(new_folder)
-            except:
-                os.makedirs(new_folder)   
-            copyfile(file_old_path,file_new_path)
- #file_names.append(file_new_path)
- 
+        print(file)
+        if file.endswith(EXTENSION):
+            FILE_OLD_PATH = os.path.join(root, file)
+            FILE_NEW_PATH = FILE_OLD_PATH.replace(SOURCE_PATH, DESTINATION_PATH)
+            NEW_FOLDER = os.path.dirname(FILE_NEW_PATH)
+            print(NEW_FOLDER)
 
-''' #ALTERNATIVE WAY 
+            try:
+                os.stat(NEW_FOLDER)
+            except ValueError:
+                os.makedirs(NEW_FOLDER)
+            copyfile(FILE_OLD_PATH, FILE_NEW_PATH)
+#FILE_NAMES.append(FILE_NEW_PATH)
+
+'''#ALTERNATIVE WAY
 for dirname, dirnames, filenames in os.walk(source_path):
     # print path to all subdirectories first.
     for subdirname in dirnames:
         for filename in filenames:
-            file_names.append(os.path.join(dirname, filename))
+            FILE_NAMES.append(os.path.join(dirname, filename))
 
 '''
 
-print(file_names)
-print (str(len(file_names)))
+print(FILE_NAMES)
+print(str(len(FILE_NAMES)))
